d9d2e4dab0817ec845fa900b8b795950339260c4
Nate Cavanaugh <nathan.cavanaugh@liferay.com>
2014-05-13 11:06:52 -0700

    Adding checking for aggressive border unsetting

--- index.js
+++ index.js
@@ -67,6 +67,7 @@ var REGEX_PROPERTY = /^\t*([^:]+:|@include\s)[^;]+;$/;
 var REGEX_SUB = /\{\s*([^|}]+?)\s*(?:\|([^}]*))?\s*\}/g;
 // var REGEX_ZERO_UNIT = /\b0(?!s\b)[a-zA-Z]{1,}\b/;
 var REGEX_INTEGER_DECIMAL = /([^0-9])(\.\d+)/;
+var REGEX_BORDER_UNSET = /border:\s?(none|0);/;
 var REGEX_DOUBLE_QUOTES = /"[^"]*"/;
 var REGEX_SINGLE_QUOTES = /'[^']*'/g;
 var REGEX_REGEX = /\/[^\/]+\//g;
@@ -93,6 +94,10 @@ var hasLowerCaseRegex = function(item) {
 	}
 
 	return lowerCaseRegex;
+};
+
+var hasInvalidBorderReset = function(item) {
+	return REGEX_BORDER_UNSET.test(item);
 };
 
 var hasRedundantRegex = function(item) {
@@ -198,6 +203,10 @@ var checkCss = function(contents, file) {
 					if (itemMatch[1] > nextItemMatch[1]) {
 						trackErr(sub('Line: {0} Sort: {1} {2}', lineNum, item, nextItem).warn, file);
 					}
+				}
+
+				if (hasInvalidBorderReset(item)) {
+					trackErr(sub('Line: {0} You should use {2}: {1}', lineNum, item, '"border-width: 0;"'.error).warn, file);
 				}
 			}
 
