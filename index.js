2f5537ac3db292964d3d2f613d0b2f557849959d
Nate Cavanaugh <nathan.cavanaugh@liferay.com>
2014-08-14 15:01:54 -0700

    Handling property name sorting a bit better (and safer)

--- index.js
+++ index.js
@@ -739,42 +739,49 @@ var testVarNames = function(varName, lineNum, file) {
 };
 
 var naturalCompare = function(a, b, caseInsensitive) {
-	if (caseInsensitive === true) {
-		a = a.toLowerCase();
-		b = b.toLowerCase();
+	var result;
+
+	if (A.Lang.isNumber(a) && A.Lang.isNumber(b)) {
+		result = (a - b);
 	}
+	else {
+		if (caseInsensitive === true) {
+			a = a.toLowerCase();
+			b = b.toLowerCase();
+		}
 
-	var aa = a.split(re.REGEX_DIGITS);
-	var bb = b.split(re.REGEX_DIGITS);
+		var aa = a.split(re.REGEX_DIGITS);
+		var bb = b.split(re.REGEX_DIGITS);
 
-	var length = Math.max(aa.length, bb.length);
+		var length = Math.max(aa.length, bb.length);
 
-	var result = 0;
+		result = 0;
 
-	for (var i = 0; i < length; i++) {
-		var itemA = aa[i];
-		var itemB = bb[i];
+		for (var i = 0; i < length; i++) {
+			var itemA = aa[i];
+			var itemB = bb[i];
 
-		if (itemA != itemB) {
-			var cmp1 = parseInt(itemA, 10);
-			var cmp2 = parseInt(itemB, 10);
+			if (itemA != itemB) {
+				var cmp1 = parseInt(itemA, 10);
+				var cmp2 = parseInt(itemB, 10);
 
-			if (isNaN(cmp1)) {
-				cmp1 = itemA;
+				if (isNaN(cmp1)) {
+					cmp1 = itemA;
+				}
+
+				if (isNaN(cmp2)) {
+					cmp2 = itemB;
+				}
+
+				if (typeof cmp1 == 'undefined' || typeof cmp2 == 'undefined') {
+					result = aa.length - bb.length;
+				}
+				else {
+					result = cmp1 < cmp2 ? -1 : 1;
+				}
+
+				break;
 			}
-
-			if (isNaN(cmp2)) {
-				cmp2 = itemB;
-			}
-
-			if (typeof cmp1 == 'undefined' || typeof cmp2 == 'undefined') {
-				result = aa.length - bb.length;
-			}
-			else {
-				result = cmp1 < cmp2 ? -1 : 1;
-			}
-
-			break;
 		}
 	}
 
@@ -845,6 +852,7 @@ var processor = {
 					var privateProp = isPrivate(propName);
 					var privatePrevProp = isPrivate(prevPropName);
 
+					var propValueFn = (item.parent.value.type == 'FunctionExpression');
 					var prevPropValueFn = (prev.parent.value.type == 'FunctionExpression');
 
 					var lifecyleMethod = (propName in LIFECYCLE_METHODS || prevPropName in LIFECYCLE_METHODS);
@@ -870,7 +878,7 @@ var processor = {
 						}
 					}
 					else {
-						if ((privateProp === privatePrevProp) && (propUpperCase === prevPropUpperCase) && naturalCompare(propName, prevPropName, true) === -1) {
+						if ((privateProp === privatePrevProp) && (propUpperCase === prevPropUpperCase) && naturalCompare(propName, prevPropName, true) === -1 && prevPropValueFn === propValueFn) {
 							needsSort = true;
 						}
 					}
