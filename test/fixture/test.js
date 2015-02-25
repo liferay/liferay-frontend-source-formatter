f7bb658a7f75e1b257eaffa27af85436f42cf830
Nate Cavanaugh <nathan.cavanaugh@liferay.com>
2015-02-25 10:22:23 -0300

    Adding tests for JS Formatter

--- test/fixture/test.js
+++ test/fixture/test.js
@@ -1,1 +1,28 @@
+/* if(){ "Hello" */
+
 var x = 'foo';
+
+if(foo){
+	alert('foo');
+} else {
+	alert('else');
+}
+
+foo({
+
+});
+
+foo(
+	function () {
+
+	}
+);
+
+Liferay.Language.get(x);
+
+console.log('foo');
+
+var something = 'foo';
+if (something) {
+
+}
