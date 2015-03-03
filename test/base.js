6a02fa8421e57d0fc635cd6358a09c3fb60af609
Nate Cavanaugh <nathan.cavanaugh@liferay.com>
2015-03-02 16:37:07 -0800

    Adding coverage for base.js

---
+++ test/base.js
@@ -0,0 +1,29 @@
+var path = require('path');
+var fs = require('fs');
+var chai = require('chai');
+var _ = require('lodash');
+
+chai.use(require('chai-string'));
+
+var assert = chai.assert;
+
+var base = require('../lib/base');
+
+describe(
+	'base',
+	function () {
+		'use strict';
+
+		var sub = base.sub;
+
+		it(
+			'should substitute strings correctly',
+			function() {
+				assert.equal('Foo bar', sub('Foo {0}', 'bar'));
+				assert.equal('Foo bar', sub('Foo {0}', ['bar']));
+				assert.equal('Foo bar', sub('Foo {key}', {key: 'bar'}));
+				assert.equal(1, sub(1, 'bar'));
+			}
+		);
+	}
+);
