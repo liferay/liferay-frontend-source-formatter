3a57372385dae0239ca952d31280e4cbc39f1c24
Nate Cavanaugh <nathan.cavanaugh@liferay.com>
2015-05-06 16:08:26 -0700

    Adding check for variable sorting within groups of individual variables

---
+++ test/lint_rules/sort_vars.js
@@ -0,0 +1,30 @@
+var path = require('path');
+
+var lint = require('../../lib/lint');
+
+var linter = lint.linter;
+var ESLintTester = require('eslint-tester');
+var eslintTester = new ESLintTester(linter);
+
+eslintTester.addRuleTest(
+	path.resolve(__dirname, '../', '../', 'lib/lint_rules/' + path.basename(__filename)),
+	{
+		valid: [
+			'var xyz = 123;',
+			'var abc = 123;\nvar def = 456;',
+			'var abc = 123; var def = 456;',
+			'var cde = 123;\nvar def = 123;\n\nvar abc = 456;',
+		],
+
+		invalid: [
+			{
+				code: 'var def = 456;\nvar abc = 123;',
+				errors: [ { message: 'Sort variables: def abc' } ]
+			},
+			{
+				code: 'var def = 456;\n\nvar def_xyz = "FOO";\nvar abc = 123;',
+				errors: [ { message: 'Sort variables: def_xyz abc' } ]
+			}
+		]
+	}
+);
