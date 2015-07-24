b119f3fdd97dabd2c7a58c037601a60ed9653eec
Nate Cavanaugh <nathan.cavanaugh@liferay.com>
2015-07-24 13:14:05 -0700

    Fixing the checking for undefined variables in JSPs. Functions can be defined in script blocks and unused, because they can be used globally, but other variables should still be searched for

---
+++ test/lint_rules/no_unused_vars.js
@@ -0,0 +1,34 @@
+var path = require('path');
+
+var lint = require('../../lib/lint');
+
+var linter = lint.linter;
+var ESLintTester = require('eslint-tester');
+var eslintTester = new ESLintTester(linter);
+
+var args = [2, {'jsp': true}];
+
+eslintTester.addRuleTest(
+	path.resolve(__dirname, '../', '../', 'lib/lint_rules/' + path.basename(__filename)),
+	{
+		valid: [
+			'var _PN_xyz = 1;',
+			{
+				code: 'var _PN_xyz = function(){}',
+				args: args
+			},
+			{
+				code: 'function _PN_xyz(){}',
+				args: args
+			},
+		],
+
+		invalid: [
+			{
+				code: 'var _PN_xyz = 1;',
+				errors: [ { message: '_PN_xyz is defined but never used' } ],
+				args: args
+			}
+		]
+	}
+);
