d5a4c96d6810bf76bd46e8aa32332feef633514b
Nate Cavanaugh <nathan.cavanaugh@liferay.com>
2016-03-08 06:22:48 -0800

    Update constants reference checking to be more robust

--- test/lint_rules/sort_constants.js
+++ test/lint_rules/sort_constants.js
@@ -14,7 +14,9 @@ ruleTester.run(
 		valid: [
 			'var ABC = 123;\n\nvar DEF = 456;',
 			'var DEF = 456;\n\nvar ABC = "FOO" + DEF;',
-			'var DEF = 456;\n\nvar GHI = 789;\n\nvar ABC = DEF;'
+			'var DEF = 456;\n\nvar GHI = 789;\n\nvar ABC = DEF;',
+			'var DEF = 456;\n\nvar ABC = some.method[DEF];',
+			'var DEF = function(){};\n\nvar ABC = DEF();'
 		],
 
 		invalid: [
@@ -25,13 +27,15 @@ ruleTester.run(
 			{
 				code: 'var DEF = 456;\n\nvar DEF_XYZ = "FOO";\n\nvar ABC = 123;',
 				errors: [ { message: 'Sort constants: DEF_XYZ ABC' } ]
+			},
+			{
+				code: 'var DEF = 456;\n\nvar ABC = "DEF";',
+				errors: [ { message: 'Sort constants: DEF ABC' } ]
+			},
+			{
+				code: 'var DEF = 456;\n\nvar ABC;',
+				errors: [ { message: 'Sort constants: DEF ABC' } ]
 			}
-			// ,
-			// Should add in the future
-			// {
-			// 	code: 'var DEF = 456;\n\nvar ABC = "DEF";',
-			// 	errors: [ { message: 'Sort constants: DEF ABC' } ]
-			// },
 		]
 	}
 );
