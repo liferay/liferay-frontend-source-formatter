df10ce11a682bbbfd863357e5571055022033da5
Nate Cavanaugh <nathan.cavanaugh@liferay.com>
2016-03-08 07:28:45 -0800

    Add support for checking var order in destructured object assignment

--- test/lint_rules/sort_vars.js
+++ test/lint_rules/sort_vars.js
@@ -7,6 +7,15 @@ var RuleTester = lint.eslint.RuleTester;
 
 var ruleTester = new RuleTester();
 
+var addES6 = function(item, index) {
+	item.parserOptions = {
+		ecmaVersion: 6,
+		ecmaFeatures: {}
+	};
+
+	return item;
+};
+
 ruleTester.run(
 	path.basename(__filename, '.js'),
 	require('../../lib/lint_rules/' + path.basename(__filename)),
@@ -18,7 +27,12 @@ ruleTester.run(
 			'var cde = 123;\nvar def = 123;\n\nvar abc = 456;',
 			'for (var i = 0; i < 10; i++) {\nvar current = 1;\n}',
 			'for (var i in obj) {\nvar current = 1;\n}'
-		],
+		].concat(
+			[
+				{ code: 'const {bar, foo} = refs;' },
+				{ code: 'const [foo, bar] = refs;' }
+			].map(addES6)
+		),
 
 		invalid: [
 			{
@@ -29,6 +43,13 @@ ruleTester.run(
 				code: 'var def = 456;\n\nvar def_xyz = "FOO";\nvar abc = 123;',
 				errors: [ { message: 'Sort variables: def_xyz abc' } ]
 			}
-		]
+		].concat(
+			[
+				{
+					code: 'const {foo, bar} = refs',
+					errors: [ { message: 'Sort variables: foo bar' } ]
+				}
+			].map(addES6)
+		)
 	}
 );
