c29a3c53e1bb797a81a1542ea7b6f37699e4fe0a
Nate Cavanaugh <nathan.cavanaugh@liferay.com>
2017-05-19 12:21:19 -0700

    Deprecating check_sf command name to use csf instead, as well as adding a deprecation warning so people can switch

--- bin/index.js
+++ bin/index.js
@@ -1,16 +1,53 @@
 #!/bin/sh
 ":" //# http://sambal.org/?p=1014 ; exec /usr/bin/env node --harmony "$0" "$@"
 
+var _ = require('lodash');
+var colors = require('cli-color-keywords')();
+var ConfigStore = require('configstore');
+var path = require('path');
 var updateNotifier = require('update-notifier');
+var fs = require('fs');
 
-var notifier = updateNotifier(
-	{
-		pkg: require('../package.json')
-	}
-);
+const ONE_DAY = 1000 * 60 * 60 * 24;
+
+var pkg = require('../package.json');
+
+var notifier = updateNotifier({pkg});
 
 if (notifier.update) {
 	notifier.notify();
 }
 
-require('../lib/cli').init();
+var cli = require('../lib/cli').init();
+
+var config = new ConfigStore(
+	pkg.name,
+	{
+		lastPackageVersion: pkg.version,
+		lastUpdateCheck: Date.now()
+	}
+);
+
+var lastUpdateCheck = config.get('lastUpdateCheck');
+
+if (path.basename(process.argv[1]) === 'check_sf' && (Date.now() - lastUpdateCheck > ONE_DAY)) {
+	var unindent = (strings, ...keys) => _.zipWith(strings, keys, _.add).join('').replace(/^\s+/gm, '  ');
+
+	cli.then(
+		function() {
+			var header = `Using the ${colors.inverse('check_sf')} form of this module is deprecated.`;
+
+			var footer = `Please use the ${colors.inverse('csf')} command instead. It's easier to type too!`;
+
+			var maxLineLength = Math.max(header.length, footer.length);
+
+			var bar = new Array(maxLineLength).join('-');
+
+			console.log(unindent`${bar}
+				${colors.error(header)}
+				${colors.green(footer)}`);
+
+			config.set('lastUpdateCheck', Date.now());
+		}
+	);
+}
