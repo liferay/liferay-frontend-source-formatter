5c8fc5ec71087fe7dbeb9d07f53f1ca5b544f7ea
Nate Cavanaugh <nathan.cavanaugh@liferay.com>
2017-12-11 15:54:38 -0800

    Checks for the existence of an initialized value before checking the type, not sure why this errors in the test only when the sourceType is module, but happens in the tool whether it's script or module. Probably needs more investigation, but could be an eslint thing. Fixes #22

--- test/lint_js_rules/no_unused_vars.js
+++ test/lint_js_rules/no_unused_vars.js
@@ -34,6 +34,14 @@ ruleTester.run(
 				code: '(function(){ var _PN_xyz = 1; });',
 				errors: [ { message: "'_PN_xyz' is assigned a value but never used." } ],
 				options: options
+			},
+			{
+				code: 'var OSBForm_EL_EXPRESSION_13;',
+				options: options,
+				parserOptions: {
+					sourceType: 'module'
+				},
+				errors: [ { message: "'OSBForm_EL_EXPRESSION_13' is defined but never used." } ],
 			}
 		]
 	}
